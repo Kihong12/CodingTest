1af683e0e131cbe97bad3c30c2da9f1a211b9464
FindTree <clevermind2@naver.com>
2025-01-27 16:02:42 +0900

    [level 0] Title: 배열 뒤집기, Time: 0.01 ms, Memory: 90.2 MB -BaekjoonHub

--- 프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.java
+++ 프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.java
@@ -1,20 +1,9 @@
-import java.util.*;
-
 class Solution {
     public int[] solution(int[] num_list) {
-        List<Integer> list = new ArrayList<>();
-        
-        for (int num : num_list) {
-            list.add(num);
-        }
-        
-        Collections.reverse(list);
-        
         int[] answer = new int[num_list.length];
         for (int i = 0; i < num_list.length; i++) {
-            answer[i] = list.get(i);
+            answer[i] = num_list[num_list.length-i-1];
         }
-        
         return answer;
     }
 }
