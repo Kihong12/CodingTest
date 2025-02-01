39dd68cb8423fc843b4119496252d9f0a0c9abd7
FindTree <clevermind2@naver.com>
2025-02-01 22:33:01 +0900

    [level 0] Title: 약수 구하기, Time: 0.33 ms, Memory: 83.7 MB -BaekjoonHub

--- 프로그래머스/0/120897. 약수 구하기/약수 구하기.java
+++ 프로그래머스/0/120897. 약수 구하기/약수 구하기.java
@@ -1,23 +1,21 @@
-import java.util.ArrayList;
-import java.util.List;
-
 class Solution {
     public int[] solution(int n) {
-        List<Integer> divisors = new ArrayList<>();
-        
-        // 1부터 n까지 반복하여 n의 약수를 찾음
+        int[] answer = new int[n];
+        int cnt = 0;
         for (int i = 1; i <= n; i++) {
-            if (n % i == 0) {  // 약수일 경우
-                divisors.add(i);
+            if (n % i == 0) {
+                cnt++;
             }
         }
-        
-        // List를 int[] 배열로 변환
-        int[] result = new int[divisors.size()];
-        for (int i = 0; i < divisors.size(); i++) {
-            result[i] = divisors.get(i);
+
+        int[] result = new int[cnt];
+        cnt = 0;
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0) {
+                result[cnt] = i;
+                cnt++;
+            }
         }
-        
         return result;
     }
-}
+}   
