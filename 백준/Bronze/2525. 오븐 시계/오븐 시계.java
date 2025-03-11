7ce248b155a415bfee89b6a46b7d91e1ec0487fd
FindTree <clevermind2@naver.com>
2025-03-11 10:31:40 +0900

    [Bronze III] Title: 오븐 시계, Time: 192 ms, Memory: 18480 KB -BaekjoonHub

---
+++ 백준/Bronze/2525. 오븐 시계/오븐 시계.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int hh = sc.nextInt();
+        int mm = sc.nextInt();
+        int timer = sc.nextInt();
+        mm += timer;
+        hh += mm / 60;
+        mm %= 60;
+        hh %= 24;
+
+        System.out.println(hh + " " + mm);
+    }
+}
