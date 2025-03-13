7e8c7a5762058598ac851b842877aebc9c0e9a55
FindTree <clevermind2@naver.com>
2025-03-13 09:45:06 +0900

    [Bronze III] Title: 최소, 최대, Time: 2272 ms, Memory: 320820 KB -BaekjoonHub

---
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        Arrays.sort(arr);
+        System.out.print(arr[0] + " ");
+        System.out.println(arr[n-1]);
+
+        sc.close();
+    }
+}
