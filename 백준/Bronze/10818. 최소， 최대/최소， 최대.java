e5e1c8cec9112825efe104a625f93a0d94d969c8
FindTree <clevermind2@naver.com>
2025-03-13 10:07:31 +0900

    [Bronze III] Title: 최소, 최대, Time: 1548 ms, Memory: 317392 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.java
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -5,16 +5,20 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int[] arr = new int[n];
 
+        int[] arr = new int[n];
         for (int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
         }
 
-        Arrays.sort(arr);
-        System.out.print(arr[0] + " ");
-        System.out.println(arr[n-1]);
-
-        sc.close();
+        int min = 0, max = 0;
+        for (int i = 1; i < n; i++) {
+            if (arr[i] < arr[min]) {
+                min = i;
+            } else if (arr[i] > arr[max]) {
+                max = i;
+            }
+        }
+        System.out.println(arr[min] + " " + arr[max]);
     }
 }
