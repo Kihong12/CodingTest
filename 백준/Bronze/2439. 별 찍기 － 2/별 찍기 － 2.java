a57e438b883236beba5f0b824a3fa35860cb8c6a
kihong <clevermind2@naver.com>
2025-08-06 10:32:44 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 104 ms, Memory: 14108 KB -BaekjoonHub

--- 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
@@ -1,12 +1,18 @@
-import java.util.Scanner;
+import java.io.*;
 
-public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+public class Main{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        for (int i = 1; i <= n; i++) {
-            System.out.println(" ".repeat(n-i) + "*".repeat(i));
+        int a = Integer.parseInt(br.readLine());
+
+        for (int i = 1; i <= a; i++) {
+            bw.write(" ".repeat(a-i));
+            bw.write("*".repeat(i));
+            bw.newLine();
         }
+        bw.flush();
+        bw.close();
     }
 }
