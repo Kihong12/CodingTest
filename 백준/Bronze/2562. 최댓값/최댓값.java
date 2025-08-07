793b7993bdf2d8cc9963b0aff183bf71f0bb57b6
kihong <clevermind2@naver.com>
2025-08-07 20:05:22 +0900

    [Bronze III] Title: 최댓값, Time: 100 ms, Memory: 14096 KB -BaekjoonHub

--- 백준/Bronze/2562. 최댓값/최댓값.java
+++ 백준/Bronze/2562. 최댓값/최댓값.java
@@ -1,21 +1,30 @@
-import java.util.Scanner;
+import java.io.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         int[] arr = new int[9];
-
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = sc.nextInt();
-        }
-
         int max = 0;
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] > arr[max]) {
-                max = i;
+        int count = 1;
+
+        for (int i = 0; i < 9; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+            if (arr[i] > max) {
+                max = arr[i];
             }
         }
-        System.out.println(arr[max]);
-        System.out.println(max+1);
+
+        for (int i = 0; i < 9; i++) {
+            if (max != arr[i]) {
+                count++;
+            } else {
+                break;
+            }
+        }
+
+        System.out.println(max);
+        System.out.println(count);
+
     }
 }
