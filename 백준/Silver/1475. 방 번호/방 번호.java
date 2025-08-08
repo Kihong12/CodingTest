d4c31877cb5f17e4b87da76a22a31b7a97afaa74
kihong <clevermind2@naver.com>
2025-08-08 23:24:53 +0900

    [Silver V] Title: 방 번호, Time: 104 ms, Memory: 14280 KB -BaekjoonHub

---
+++ 백준/Silver/1475. 방 번호/방 번호.java
@@ -0,0 +1,31 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String b = br.readLine();
+
+        int[] arr = new int[10];
+
+        for (int i = 0; i < b.length(); i++) {
+            int num = b.charAt(i) - 48;
+            if (num == 6) {
+                arr[9]++;
+            } else {
+                arr[num]++;
+            }
+        }
+
+        int max = 0;
+        for (int i = 0; i < 9; i++) {
+            max = Math.max(max, arr[i]);
+        }
+
+        int nine = arr[9] / 2;
+        if (arr[9] % 2 == 1) nine++;
+
+        max = Math.max(max, nine);
+        System.out.println(max);
+    }
+}
